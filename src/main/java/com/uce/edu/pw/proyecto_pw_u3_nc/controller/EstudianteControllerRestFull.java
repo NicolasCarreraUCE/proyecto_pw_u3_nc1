94785453f97147c2ab8b71af6089c8e2fd79cff7
GeoWillC <nacarrera@uce.edu.ec>
2023-02-04 10:56:48 -0500

    taller 27

--- src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/controller/EstudianteControllerRestFull.java
+++ src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/controller/EstudianteControllerRestFull.java
@@ -1,8 +1,11 @@
 package com.uce.edu.pw.proyecto_pw_u3_nc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,27 +13,41 @@ import com.uce.edu.pw.proyecto_pw_u3_nc.modelo.Estudiante;
 import com.uce.edu.pw.proyecto_pw_u3_nc.service.IEstudainteService;
 
 @RestController
-@RequestMapping("/estudainte")
+@RequestMapping("/estudaintes")
 public class EstudianteControllerRestFull {
 
     @Autowired
     private IEstudainteService estudainteService;
 	
+	@PostMapping
     public void registrar(Estudiante estudiante) {
 		// TODO Auto-generated method stub
 	}
 
-    public void actualizar(Estudiante estudiante) {
+	@PutMapping(path = "/{id}")
+    public void actualizar(@PathVariable("id") Estudiante estudiante) {
 		// TODO Auto-generated method stub
 	}
 
-    @GetMapping(path = "/buscar/{id}")
+	@PutMapping
+    public void actualizarTodos(Estudiante estudiante) {
+		// TODO Auto-generated method stub
+	}
+
+    @GetMapping(path = "/{id}")
 	public Estudiante encontrar(@PathVariable("id") Integer id) {
 		// TODO Auto-generated method stub
         return this.estudainteService.encontrar(id);
 	}
 
-	public void borrar(Integer id) {
+	@DeleteMapping(path = "/{id}")
+	public void borrar(@PathVariable("id") Integer id) {
+		// TODO Auto-generated method stub
+		this.estudainteService.borrar(id);
+	}
+
+	@DeleteMapping
+	public void borrarTodos(Integer id) {
 		// TODO Auto-generated method stub
 	}
 }

53936438ff3e5963c410663b7faac09a25747557
Nicolas Carrera <nacarrera@uce.edu.ec>
2023-02-09 02:07:45 -0500

    tarea 15

--- src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/controller/ProfesorControllerRestFull.java
+++ src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/controller/ProfesorControllerRestFull.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,4 +34,16 @@ public class ProfesorControllerRestFull {
 	public void borrar(@PathVariable("id") Integer id) {
         this.profesorServi.borrar(id);
     }
+
+    @PostMapping
+    public void registrar(@RequestBody Profesor profesor) {
+		// TODO Auto-generated method stub
+		this.profesorServi.registrar(profesor);
+	}
+
+    @PutMapping(path = "/{id}")
+    public void actualizar(@PathVariable("id") Integer id , @RequestBody Profesor profesor) {
+		// TODO Auto-generated method stub
+		this.profesorServi.actualizar(profesor);
+	}
 }

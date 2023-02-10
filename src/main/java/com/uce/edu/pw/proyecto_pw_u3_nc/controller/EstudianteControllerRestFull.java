694fae324071d57f49b2fe3a7ec7ec2306f77d27
GeoWillC <nacarrera@uce.edu.ec>
2023-02-09 19:44:31 -0500

    taller 29

--- src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/controller/EstudianteControllerRestFull.java
+++ src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/controller/EstudianteControllerRestFull.java
@@ -4,6 +4,10 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,15 +52,27 @@ public class EstudianteControllerRestFull {
 	}
 
     @GetMapping(path = "/{id}")
-	public Estudiante encontrar(@PathVariable("id") Integer id) {
+	public ResponseEntity<Estudiante> encontrar(@PathVariable("id") Integer id) {
 		// TODO Auto-generated method stub
-        return this.estudainteService.encontrar(id);
+    	Estudiante estudiante = this.estudainteService.encontrar(id);
+    	int codigoMensaje = 0;
+    	if (true) {
+			codigoMensaje = 230;
+			return ResponseEntity.status(codigoMensaje).body(estudiante); 
+		}
+    	return ResponseEntity.ok(estudiante);
+//    	return ResponseEntity.status(227).body(estudiante); 
+//    	return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(estudiante); 
 	}
 
 	@GetMapping
-	public List<Estudiante> encontrarTodos() {
+	public ResponseEntity<List<Estudiante>> encontrarTodos() {
 		// TODO Auto-generated method stub
-        return this.estudainteService.encontrarTodos();
+    	HttpHeaders cabeceras = new HttpHeaders();
+    	cabeceras.add("detalleMensaje", "Estudaine encontrado correctacmente");
+    	cabeceras.add("valorCalculado", "100");
+		List<Estudiante> lista = this.estudainteService.encontrarTodos();
+    	return new ResponseEntity<>(lista, cabeceras, 230);
 	}
 
 	@GetMapping(path = "/salario")

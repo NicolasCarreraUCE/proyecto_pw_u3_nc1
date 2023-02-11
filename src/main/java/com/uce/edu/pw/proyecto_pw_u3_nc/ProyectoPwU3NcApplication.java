e1c7b03fadc4aaca897713ccf28f8d1d2e0dc278
Nicolas Carrera <nacarrera@uce.edu.ec>
2023-02-11 01:43:10 -0500

    tarea 16 backend

--- src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/ProyectoPwU3NcApplication.java
+++ src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/ProyectoPwU3NcApplication.java
@@ -2,12 +2,25 @@ package com.uce.edu.pw.proyecto_pw_u3_nc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProyectoPwU3NcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoPwU3NcApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+			}
+		};
 	}
 
 }

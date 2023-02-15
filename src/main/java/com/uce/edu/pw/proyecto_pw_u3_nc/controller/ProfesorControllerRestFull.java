9f5d169cbf49dfe32786610fafa5f9e0c78e9cc3
Nicolas Carrera <nacarrera@uce.edu.ec>
2023-02-15 00:18:11 -0500

    tarea17

--- src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/controller/ProfesorControllerRestFull.java
+++ src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/controller/ProfesorControllerRestFull.java
@@ -1,9 +1,15 @@
 package com.uce.edu.pw.proyecto_pw_u3_nc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,38 +38,38 @@ public class ProfesorControllerRestFull {
 //        this.profesorServi.borrar(id);
 //    }
 
-    // Capacidad que reciba un Media Type application/json
-    @PutMapping(path = "/recibir/json", consumes = (MediaType.APPLICATION_JSON_VALUE))
+    // Capacidad que reciba un Media Type application/json 
+    @PutMapping(path = "/recibir/json", consumes = (MediaType.APPLICATION_JSON_VALUE)) 
     public void putMediaTypeJson(@RequestBody Profesor profesor){
         this.profesorServi.registrar(profesor);
     }
-    // Capacidad de retorne un Media Type application/json
-    @GetMapping(path = "/retornar/json/{id}", produces = (MediaType.APPLICATION_JSON_VALUE))
+    // Capacidad de retorne un Media Type application/json 
+    @GetMapping(path = "/retornar/json/{id}", produces = (MediaType.APPLICATION_JSON_VALUE)) 
     public ResponseEntity<Profesor> getMediaTypeJson(@PathVariable("id") Integer id){
         Profesor profesor = this.profesorServi.encontrar(id);
         return ResponseEntity.status(HttpStatus.OK).body(profesor);
     }
-    // Capacidad que reciba un Media Type application/xml
-    @PutMapping(path = "/recibir/xml", consumes = (MediaType.APPLICATION_XML_VALUE))
+    // Capacidad que reciba un Media Type application/xml 
+    @PutMapping(path = "/recibir/xml", consumes = (MediaType.APPLICATION_XML_VALUE)) 
     public void putMediaTypeXml(@RequestBody Profesor profesor){
         this.profesorServi.registrar(profesor);
     }
-    // Capacidad de retorne un Media Type application/xml
-    @GetMapping(path = "/retornar/xml/{id}", produces = (MediaType.APPLICATION_XML_VALUE))
+    // Capacidad de retorne un Media Type application/xml 
+    @GetMapping(path = "/retornar/xml/{id}", produces = (MediaType.APPLICATION_XML_VALUE)) 
     public ResponseEntity<Profesor> getMediaTypeXlm(@PathVariable("id") Integer id){
         Profesor profesor = this.profesorServi.encontrar(id);
         return ResponseEntity.status(HttpStatus.OK).body(profesor);
     }
-    // Capacidad que reciba y retorne un Media Type application/json
-    @PostMapping(path = "/recibir/retornar/json/{id}", consumes = (MediaType.APPLICATION_JSON_VALUE), produces = (MediaType.APPLICATION_JSON_VALUE))
+    // Capacidad que reciba y retorne un Media Type application/json 
+    @PostMapping(path = "/recibir/retornar/json/{id}", consumes = (MediaType.APPLICATION_JSON_VALUE), produces = (MediaType.APPLICATION_JSON_VALUE)) 
     public ResponseEntity<Profesor> putAndGetMediaTypeJson(@PathVariable("id") Integer id , @RequestBody Profesor profesor) {
 		profesor.setId(id);
 		this.profesorServi.actualizar(profesor);
 		Profesor prof = this.profesorServi.encontrar(id);
 		return ResponseEntity.status(HttpStatus.OK).body(prof); 
 	}
-    // Capacidad que reciba y retorne un Media Type application/xml
-    @PostMapping(path = "/recibir/retornar/json/{id}", consumes = (MediaType.APPLICATION_XML_VALUE), produces = (MediaType.APPLICATION_XML_VALUE))
+    // Capacidad que reciba y retorne un Media Type application/xml 
+    @PostMapping(path = "/recibir/retornar/xml/{id}", consumes = (MediaType.APPLICATION_XML_VALUE), produces = (MediaType.APPLICATION_XML_VALUE)) 
     public ResponseEntity<Profesor> putAndGetMediaTypeXml(@PathVariable("id") Integer id , @RequestBody Profesor profesor) {
 		profesor.setId(id);
 		this.profesorServi.actualizar(profesor);

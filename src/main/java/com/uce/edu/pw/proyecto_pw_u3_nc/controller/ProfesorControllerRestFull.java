cbc5ad954a09a175eb072125ccc2a3fcace69934
NicolasCarreraUCE <95258232+NicolasCarreraUCE@users.noreply.github.com>
2023-02-15 00:48:15 +0000

    tarea 17

--- src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/controller/ProfesorControllerRestFull.java
+++ src/main/java/com/uce/edu/pw/proyecto_pw_u3_nc/controller/ProfesorControllerRestFull.java
@@ -17,18 +17,57 @@ public class ProfesorControllerRestFull {
     @Autowired
     private IProfesorServi profesorServi;
 
-    @GetMapping(path = "/{id}")
-	public Profesor encontrar(@PathVariable("id") Integer id) {
-        return this.profesorServi.encontrar(id);
-    }
+//    @GetMapping(path = "/{id}")
+//	public Profesor encontrar(@PathVariable("id") Integer id) {
+//        return this.profesorServi.encontrar(id);
+//    }
+//
+//    @GetMapping(path = "/cedula/{cedula}")
+//    public Profesor encontrarPorCedula(@PathVariable("cedula") String cedula) {
+//        return this.profesorServi.encontrarPorCedula(cedula);
+//    }
+//
+//    @DeleteMapping(path = "/{id}")
+//	public void borrar(@PathVariable("id") Integer id) {
+//        this.profesorServi.borrar(id);
+//    }
 
-    @GetMapping(path = "/cedula/{cedula}")
-    public Profesor encontrarPorCedula(@PathVariable("cedula") String cedula) {
-        return this.profesorServi.encontrarPorCedula(cedula);
+    // Capacidad que reciba un Media Type application/json
+    @PutMapping(path = "/recibir/json", consumes = (MediaType.APPLICATION_JSON_VALUE))
+    public void putMediaTypeJson(@RequestBody Profesor profesor){
+        this.profesorServi.registrar(profesor);
     }
-
-    @DeleteMapping(path = "/{id}")
-	public void borrar(@PathVariable("id") Integer id) {
-        this.profesorServi.borrar(id);
+    // Capacidad de retorne un Media Type application/json
+    @GetMapping(path = "/retornar/json/{id}", produces = (MediaType.APPLICATION_JSON_VALUE))
+    public ResponseEntity<Profesor> getMediaTypeJson(@PathVariable("id") Integer id){
+        Profesor profesor = this.profesorServi.encontrar(id);
+        return ResponseEntity.status(HttpStatus.OK).body(profesor);
     }
+    // Capacidad que reciba un Media Type application/xml
+    @PutMapping(path = "/recibir/xml", consumes = (MediaType.APPLICATION_XML_VALUE))
+    public void putMediaTypeXml(@RequestBody Profesor profesor){
+        this.profesorServi.registrar(profesor);
+    }
+    // Capacidad de retorne un Media Type application/xml
+    @GetMapping(path = "/retornar/xml/{id}", produces = (MediaType.APPLICATION_XML_VALUE))
+    public ResponseEntity<Profesor> getMediaTypeXlm(@PathVariable("id") Integer id){
+        Profesor profesor = this.profesorServi.encontrar(id);
+        return ResponseEntity.status(HttpStatus.OK).body(profesor);
+    }
+    // Capacidad que reciba y retorne un Media Type application/json
+    @PostMapping(path = "/recibir/retornar/json/{id}", consumes = (MediaType.APPLICATION_JSON_VALUE), produces = (MediaType.APPLICATION_JSON_VALUE))
+    public ResponseEntity<Profesor> putAndGetMediaTypeJson(@PathVariable("id") Integer id , @RequestBody Profesor profesor) {
+		profesor.setId(id);
+		this.profesorServi.actualizar(profesor);
+		Profesor prof = this.profesorServi.encontrar(id);
+		return ResponseEntity.status(HttpStatus.OK).body(prof); 
+	}
+    // Capacidad que reciba y retorne un Media Type application/xml
+    @PostMapping(path = "/recibir/retornar/json/{id}", consumes = (MediaType.APPLICATION_XML_VALUE), produces = (MediaType.APPLICATION_XML_VALUE))
+    public ResponseEntity<Profesor> putAndGetMediaTypeXml(@PathVariable("id") Integer id , @RequestBody Profesor profesor) {
+		profesor.setId(id);
+		this.profesorServi.actualizar(profesor);
+		Profesor prof = this.profesorServi.encontrar(id);
+		return ResponseEntity.status(HttpStatus.OK).body(prof); 
+	}
 }
